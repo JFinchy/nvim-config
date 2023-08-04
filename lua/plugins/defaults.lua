6cf0b267fd51ca9f2bc3b064696c00a27666b3e3
jfinch22 <JacobFinch@rockethomes.com>
2023-08-04 13:48:32 -0700

    init

---
+++ lua/plugins/defaults.lua
@@ -0,0 +1,22 @@
+local wk = require("which-key")
+
+wk.register({
+  t = {
+    name = "Todo",
+    q = { ":TodoQuickFix", "Quick Fix" },
+    t = { ":TodoTelescope", "Telescope" },
+  },
+  G = {
+    name = "Grep",
+    d = { ":GrepRoot ", "Root directory" },
+    r = { ":Replace", "Replace" },
+    s = { "Grep ", "Text" },
+  },
+  H = {
+    name = "Harpoon",
+    a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Add File" },
+    f = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Marks File" },
+    n = { "<cmd>lua require('harpoon.ui').nav_next()<cr>", "Next" },
+    p = { "<cmd>lua require('harpoon.ui').nav_prev()<cr>", "Prev" },
+  },
+}, { prefix = "<leader>" })
