96ad3006c8a382074ee60ae6b96f7c206c59cd93
Jake Finch <JacobDavidFinch@gmail.com>
2023-08-08 20:35:57 -0700

    chore: drop mark plugin for now

--- lua/plugins/defaults.lua
+++ lua/plugins/defaults.lua
@@ -6,11 +6,4 @@ wk.register({
     q = { ":TodoQuickFix", "Quick Fix" },
     t = { ":TodoTelescope", "Telescope" },
   },
-  H = {
-    name = "Harpoon",
-    a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Add File" },
-    f = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Marks File" },
-    n = { "<cmd>lua require('harpoon.ui').nav_next()<cr>", "Next" },
-    p = { "<cmd>lua require('harpoon.ui').nav_prev()<cr>", "Prev" },
-  },
 }, { prefix = "<leader>" })
