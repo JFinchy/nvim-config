22fbbb7064b95f5f94ed1159306c331052de2175
Jake Finch <JacobDavidFinch@gmail.com>
2023-08-13 20:59:16 -0700

    feat: worktree

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,4 +2,5 @@ return {
   { "sindrets/diffview.nvim" },
   -- FIXME: { "gitsigns" },
   { "tanvirtin/vgit.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "ThePrimeagen/git-worktree.nvim" },
 }
