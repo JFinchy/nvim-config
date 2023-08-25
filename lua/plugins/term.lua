49f104fa2481a29bd9c80de079970eec90ca2c32
jfinch22 <JacobFinch@rockethomes.com>
2023-08-25 08:00:51 -0700

    latest

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -1,7 +1,13 @@
 return {
   {
     "akinsho/toggleterm.nvim",
-    keys = {},
+    keys = {
+      {
+        "<leader>td",
+        "<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>",
+        desc = "Open a horizontal terminal at the Desktop directory",
+      },
+    },
     config = function()
       require("toggleterm").setup()
     end,
